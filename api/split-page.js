690ac330d768ca5f56d1bb8b59e4b8c6319b4f6b
robaeprice <robaeprice@gmail.com>
2026-03-20 16:59:38 -0400

    Update split-page.js

--- api/split-page.js
+++ api/split-page.js
@@ -3,31 +3,61 @@ module.exports = async (req, res) => {
     const protocol = req.headers['x-forwarded-proto'] || 'https';
     const host = req.headers['host'];
     const origin = `${protocol}://${host}`;
+    const splitId = req.query.id || '';
+
+    // Fetch group name from Redis
+    let ogTitle = 'Join my Group Split on Comrade!';
+    try {
+      const splitRes = await fetch(process.env.UPSTASH_REDIS_REST_URL, {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${process.env.UPSTASH_REDIS_REST_TOKEN}`,
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(['GET', splitId])
+      });
+      const splitData = await splitRes.json();
+      if (splitData.result) {
+        const parsed = JSON.parse(splitData.result);
+        if (parsed.groupName) {
+          ogTitle = `Join ${parsed.groupName} on Comrade!`;
+        }
+      }
+    } catch (e) {
+      // Fall back to generic title
+    }
 
     const response = await fetch(`${origin}/index.html`);
     if (!response.ok) throw new Error('Failed to fetch index.html');
     let html = await response.text();
 
-    const splitId = req.query.id || '';
     const splitUrl = `${origin}/split/${splitId}`;
 
     // Replace <title>
-   html = html.replace(
-  /<title>.*?<\/title>/i,
-  '<title>Join my Group Split on Comrade!</title>'
-);
+    html = html.replace(
+      /<title>.*?<\/title>/i,
+      `<title>${ogTitle}</title>`
+    );
 
     // Replace og:title
-  html = html.replace(
-  /<meta\s+property=["']og:title["'][^>]*>/i,
-  '<meta property="og:title" content="Join my Group Split on Comrade!" />'
-);
-
     html = html.replace(
-  /<meta name="twitter:title" content=".*?">/i,
-  '<meta name="twitter:title" content="Join my Group Split on Comrade!">'
-);
-    
+      /<meta\s+property=["']og:title["'][^>]*>/i,
+      `<meta property="og:title" content="${ogTitle}" />`
+    );
+
+    // Replace or insert twitter:title
+    if (html.match(/twitter:title/i)) {
+      html = html.replace(
+        /<meta name="twitter:title" content=".*?">/i,
+        `<meta name="twitter:title" content="${ogTitle}">`
+      );
+    } else {
+      html = html.replace(
+        '</head>',
+        `<meta name="twitter:title" content="${ogTitle}" />\n</head>`
+      );
+    }
+
     // Replace og:description
     html = html.replace(
       "og:description\" content=\"Wealth isn't evenly distributed. Costs shouldn't be either.\"",
@@ -36,24 +66,16 @@ module.exports = async (req, res) => {
 
     // Replace og:url
     html = html.replace(
-  /<meta\s+property=["']og:url["'][^>]*>/i,
-  `<meta property="og:url" content="${splitUrl}" />`
-);
+      /<meta\s+property=["']og:url["'][^>]*>/i,
+      `<meta property="og:url" content="${splitUrl}" />`
+    );
 
     // Replace meta description
-  html = html.replace(
-  /<meta\s+name=["']description["'][^>]*>/i,
-  '<meta name="description" content="The fairer way to split bills with friends." />'
-);
+    html = html.replace(
+      /<meta\s+name=["']description["'][^>]*>/i,
+      '<meta name="description" content="The fairer way to split bills with friends." />'
+    );
 
-    if (!html.match(/twitter:title/i)) {
-  html = html.replace(
-    '</head>',
-    '<meta name="twitter:title" content="Join my Group Split on Comrade!" />\n</head>'
-  );
-}
-
-    
     res.setHeader('Content-Type', 'text/html; charset=utf-8');
     return res.send(html);
   } catch (err) {
